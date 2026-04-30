cee6019db236e1de4213cccb693342d9713f0978
mundi morgado <mundizzle@gmail.com>
2026-04-29 22:22:42 -0700

    Fix Storybook subpath runtime assets

--- scripts/build-storybook-public.mjs
+++ scripts/build-storybook-public.mjs
@@ -36,8 +36,18 @@ async function fixManagerAssetPaths() {
   const indexPath = `${publicDir}/index.html`;
   const html = await readFile(indexPath, "utf8");
   const fixedHtml = html
+    .replaceAll("url('./sb-", "url('/storybook/sb-")
     .replaceAll('href="./sb-', 'href="/storybook/sb-')
     .replaceAll("import './sb-", "import '/storybook/sb-");
 
   await writeFile(indexPath, fixedHtml);
+
+  const globalsRuntimePath = `${publicDir}/sb-manager/globals-runtime.js`;
+  const globalsRuntime = await readFile(globalsRuntimePath, "utf8");
+  const fixedGlobalsRuntime = globalsRuntime.replaceAll(
+    'STORY_INDEX_PATH = "./index.json"',
+    'STORY_INDEX_PATH = "/storybook/index.json"',
+  );
+
+  await writeFile(globalsRuntimePath, fixedGlobalsRuntime);
 }
